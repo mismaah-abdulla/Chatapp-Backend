06ef42ba43a4c3643f256032da74c89a6353652d
mismaah.abdulla <mismaahabdulla@gmail.com>
2019-04-15 13:29:50 +0500

    Initial commit

---
+++ public/precache-manifest.febd3275042868c64227f3e951289e26.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "38b5d9d3e8d7b67ae839",
+    "url": "/css/about.fac3032a.css"
+  },
+  {
+    "revision": "38b5d9d3e8d7b67ae839",
+    "url": "/js/about.bcbdecac.js"
+  },
+  {
+    "revision": "af23e5f40d656e532e96",
+    "url": "/css/app.1a7e1eda.css"
+  },
+  {
+    "revision": "af23e5f40d656e532e96",
+    "url": "/js/app.5d5ecde5.js"
+  },
+  {
+    "revision": "8048b9a682d9bf54f8cd",
+    "url": "/css/chunk-vendors.938d8773.css"
+  },
+  {
+    "revision": "8048b9a682d9bf54f8cd",
+    "url": "/js/chunk-vendors.f038fd57.js"
+  },
+  {
+    "revision": "c98a7bd3f705e697eccfdd882d71e43b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a83841c50aa67da6144860bd5031cc53",
+    "url": "/githubLogo.png"
+  },
+  {
+    "revision": "98c0c11d4321523abb2b104d338865ba",
+    "url": "/Logo_deep_orange.png"
+  },
+  {
+    "revision": "c36e19f5f631fde23360c1ca54993c08",
+    "url": "/Logo.png"
+  },
+  {
+    "revision": "1218b2a077f40269c4a34e8af859c9da",
+    "url": "/Logo.svg"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "79c20bec10931e318a99f992097e8219",
+    "url": "/Logo.ai"
+  }
+];
