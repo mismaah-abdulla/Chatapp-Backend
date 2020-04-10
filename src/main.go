e74d71d51b5335b1f43dda19fd9c06e2c5c44b86
mismaah-abdulla <mismaahabdulla@gmail.com>
2020-04-10 21:16:03 +0800

    Use same handler

--- src/main.go
+++ src/main.go
@@ -37,13 +37,11 @@ var users = allUsers{
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/api", home)
-	fs := http.FileServer(http.Dir("../public"))
-	http.Handle("/", fs)
-	http.HandleFunc("/ws", handleConnections)
+	router.Handle("/", http.FileServer(http.Dir("../public")))
+	router.HandleFunc("/ws", handleConnections)
 	go handleMessages()
-
 	log.Println("http server started on :8000")
-	err := http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
